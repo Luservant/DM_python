5fafa195cb29339c49ecaf132b4f622834222096
lcs.servant04 <lcs.servant04@gmail.com>
2026-03-18 13:56:45 +0100

    mise_en_place

---
+++ projet_1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# df = pd.read_csv(
+#     'https://www.data.gouv.fr/fr/datasets/r/182268fc-2103-4bcb-a850-6cf90b02a9eb'
+# )
+
+df = pd.read_csv('data/elections_2022.csv')
+
+# Q1
